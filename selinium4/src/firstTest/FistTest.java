4d55f8174ab6d200858324a9a792a5b5e1af9c00
TripGain <TripGain@DESKTOP-BIQMH6D>
2021-04-28 14:32:07 +0530

    Aman:New login automation

---
+++ selinium4/src/firstTest/FistTest.java
@@ -0,0 +1,24 @@
+package firstTest;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+
+public class FistTest {
+
+	public static void main(String[] args) {
+		System.setProperty("webdriver.gecko.driver","C:/Users/aman/selinium automation/geckodriver-v0.29.1-win64/geckodriver.exe");
+		//FirefoxDriver driver=new FirefoxDriver();
+		WebDriver driver = new FirefoxDriver();
+		
+		driver.get("http://template12.iweensoft.com/indexpage.jsp");
+		driver.findElement(By.id("username")).sendKeys("Kavitha");
+		driver.findElement(By.id("password")).sendKeys("Thanks@1122");
+		driver.findElement(By.id("loginbutton")).click();
+		driver.findElement(By.id("closenotify")).click();
+		
+		
+
+	}
+
+}
